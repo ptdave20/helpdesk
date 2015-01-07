b398ead4d19c2031801bbe23ed0054ebc8040a7b
David Marchbanks <ptdave20@gmail.com>
2015-01-07 15:41:24 -0500

    Self-Creating config

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/go-martini/martini"
 	//"github.com/martini-contrib/gzip"
+	"fmt"
 	"github.com/martini-contrib/sessions"
 	"io/ioutil"
 	"log"
@@ -12,9 +13,44 @@ import (
 var CFG Config
 
 func main() {
+	println("Looking for config.json")
 	cfgBytes, err := ioutil.ReadFile("config.json")
+
+	// There was an error reading the config file, may not exist
 	if err != nil {
-		log.Fatal(err)
+		// Attempt to read the data in from the console
+
+		print("Enter your ClientID     : ")
+		var clientid, secret, host string
+		_, err := fmt.Scanf("%s\n", &clientid)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		print("Enter your Client Secret: ")
+		_, err = fmt.Scanf("%s\n", &secret)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		print("Enter your Host Name    : ")
+		_, err = fmt.Scanf("%s\n", &host)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		CFG.ClientID = clientid
+		CFG.ClientSecret = secret
+		CFG.RedirectURI = host + "/o/token"
+
+		cfgBytes, err = json.Marshal(&CFG)
+
+		err = ioutil.WriteFile("config.json", cfgBytes, 0755)
+
+		// We were unable to save, continue on anyhow
+		if err != nil {
+			println("Unable to save config")
+		}
 	}
 	err = json.Unmarshal(cfgBytes, &CFG)
 
